4663608d1f5f08ff3aaad64ee014ee64808fcb52
Doug R. Thomas <thomasdouglasr@gmail.com>
2017-06-16 14:31:30 -0500

    Create monthly-budget-hit.js

---
+++ monthly-budget-hit.js
@@ -0,0 +1,27 @@
+function main() {
+  function getBudget() { 
+    // future - pull from some place
+    budget = 874.57;
+  }
+  function getAcctCost() {
+    var currentAccount = AdWordsApp.currentAccount();
+    var stats = currentAccount.getStatsFor('THIS_MONTH');
+    monthCost = stats.getCost();
+    Logger.log('$' + monthCost + ' cost');
+  }
+  function pauseCampaign() {
+    var campaignIterator = AdWordsApp.campaigns().get();
+    while (campaignIterator.hasNext()) {
+      var campaign = campaignIterator.next();
+      campaign.pause();
+    }
+  }
+  function compareBudgets() {
+    if(monthCost > budget) {
+      pauseCampaign();
+    }
+  }
+  getBudget();
+  getAcctCost();
+  compareBudgets();
+}
